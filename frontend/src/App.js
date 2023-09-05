e78d86f9e68cbcbe78b49e1a459693df7fd9e60f
lipinghu-cu <liping.hu@enertis.com>
2023-09-04 21:07:07 -0400

    created-routes-for-home-and-product-page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,28 @@
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
-  return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>
-        <h1> Featured Products </h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.id}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p> {product.name}</p>
-                </a>
+  /*
+  
+  path="/product/:slug" the '/:slug is a placeholder for any value that appears in place of :slug' 
+  I am passing the :slug variable from the HomeScreen component's (link) anchor tag
 
-                <p>
-                  <strong> ${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+  */
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,14 @@
+import { useParams } from 'react-router-dom';
+
+//get the slug from the url and show it in the screen, use a hook
+function ProductScreen() {
+  const params = useParams();
+  const { slug } = params;
+  console.log(params);
+  return (
+    <div>
+      <h1>{slug}</h1>
+    </div>
+  );
+}
+export default ProductScreen;
