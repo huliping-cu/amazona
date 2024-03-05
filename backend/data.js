579a700ecf1ab270e5d960c3d29d898fb0ef8dcc
lipinghu-cu <lh3009@columbia.edu>
2024-03-04 22:35:28 -0500

    seed-sample-users

--- backend/data.js
+++ backend/data.js
@@ -1,5 +1,21 @@
 //create object to return sample products
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Basir',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     //each product is an object
     {
